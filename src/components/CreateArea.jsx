63d98e2e24ef1e3e23d74d254e409e09de8364af
Gagan Preet <gaganpreet.dev@gmail.com>
2021-05-13 20:22:40 +0530

    add event handeler on change of title and content

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+  const [noteInput, setNoteInput] = useState({ title: "", noteContent: "" });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    if (name === "title") {
+      setNoteInput((prevValue) => {
+        return {
+          title: event.target.value,
+          noteContent: prevValue.noteContent
+        };
+      });
+    } else if (name === "content") {
+      setNoteInput((prevValue) => {
+        return {
+          title: prevValue.noteContent,
+          noteContent: event.target.value
+        };
+      });
+    }
+
+    console.log(event.target.value);
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+          // value={noteInput.title}
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          // value={noteInput.noteContent}
+        />
         <button>Add</button>
       </form>
     </div>
